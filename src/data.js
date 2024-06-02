b1be27acec485bd5bcbed670a2577f7f6483a657
Beriraj <n.beriraj@gmail.com>
2024-06-02 16:44:30 +0530

    insert commit

---
+++ src/data.js
@@ -0,0 +1,71 @@
+import img1 from './img/img1.jpg';
+import img2 from './img/img2.jpg';
+import img3 from './img/img3.jpg';
+import img4 from './img/img4.jpg';
+import img5 from './img/img5.jpg';
+import img6 from './img/img6.jpg';
+import img7 from './img/img7.jpg';
+import img8 from './img/img8.jpg';
+
+const data={
+    productData:[
+        {
+            id: 1,
+            img: img1,
+            title: 'Apple',
+            desc: '',
+            price: 80,
+        },
+        {
+            id: 2,
+            img: img2,
+            title: 'Straw Berry',
+            desc:'',
+            price: 120,
+        },
+        {
+            id: 3,
+            img: img3,
+            title: 'Orange',
+            desc:'',
+            price: 60,
+        },
+        {
+            id: 4,
+            img: img4,
+            title: 'Grapes',
+            desc:'',
+            price: 40,
+        },
+        {
+            id: 5,
+            img: img5,
+            title: 'Guava',
+            desc:'',
+            price: 75,
+        },
+        {
+            id: 6,
+            img: img6,
+            title: 'Pine Apple',
+            desc: '',
+            price: 20,
+        },
+        {
+            id: 7,
+            img: img7,
+            title: 'Jack Fruit',
+            desc: '',
+            price: 200,
+        },
+        {
+            id: 8,
+            img: img8,
+            title: 'Pomegranate',
+            desc: '',
+            price: 100,
+        }
+    ],
+};
+
+export default data;
